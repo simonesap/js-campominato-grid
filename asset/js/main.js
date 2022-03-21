bf9517489cd2891cdd793e757292902a916cb34f
Simone Saputo <simonesaputo0@gmail.com>
2022-03-21 18:07:40 +0100

    first-commit

---
+++ asset/js/main.js
@@ -0,0 +1,15 @@
+// Consegna
+// Generare una griglia di gioco quadrata, simile a quella dello screenshot, in cui ogni cella contiene un numero tra 1 e 100.
+// Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro.
+// Bonus
+// Permettere all'utente di indicare una difficoltà in base alla quale viene generato un numero variabile di celle:
+// con difficoltà 1 => tra 1 e 100
+// con difficoltà 2 => tra 1 e 81
+// con difficoltà 3 => tra 1 e 49
+// Consigli del giorno: :party_wizard:
+// Scriviamo prima cosa vogliamo fare passo passo in italiano, dividiamo il lavoro in micro problemi.
+// Ad esempio: Di cosa ho bisogno per generare i numeri?
+// Proviamo sempre prima con dei console.log() per capire se stiamo ricevendo i dati giusti.
+// Le validazioni e i controlli possiamo farli anche in un secondo momento.
+
+for()
